fe6fdd197f4e23e20a050a7d738edbb018a71682
James <James@DESKTOP-FDSRI6B>
2023-10-19 11:14:06 +0200

    Add project files.

---
+++ WinterspringLauncher/LauncherConfig.cs
@@ -0,0 +1,74 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace WinterspringLauncher;
+
+public class LauncherConfig
+{
+    public const string DEFAULT_CONFIG_VALUE = "default";
+
+    public int ConfigVersion { get; set; } = 1;
+
+    //public string GitRepoWinterspringLauncher { get; set; } = "0blu/WinterspringLauncher";
+    public string GitRepoHermesProxy { get; set; } = "WowLegacyCore/HermesProxy";
+    public string GitRepoArctiumLauncher { get; set; } = "Arctium/WoW-Launcher";
+
+    public string WindowsGameDownloadUrl { get; set; } = DEFAULT_CONFIG_VALUE;
+    public string MacGameDownloadUrl { get; set; } = DEFAULT_CONFIG_VALUE;
+    public string GamePatcherUrl { get; set; } = DEFAULT_CONFIG_VALUE;
+
+    public string HermesProxyPath { get; set; } = "./hermes-proxy";
+    public string GamePath { get; set; } = "./World of Warcraft 1.14.0";
+    public string ArctiumLauncherPath { get; set; } = "./arctium-launcher";
+    public bool RecreateDesktopShortcut { get; set; } = !RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+    public bool AutoUpdateThisLauncher { get; set; } = false;
+
+    public string Realmlist { get; set; } = "logon.lucifron.org";
+
+    public static LauncherConfig GetDefaultConfig() => new LauncherConfig();
+
+    public void SaveConfig(string configPath)
+    {
+        var options = new JsonSerializerOptions { WriteIndented = true };
+        string jsonString = JsonSerializer.Serialize(this, options);
+        File.WriteAllText(configPath, jsonString, Encoding.UTF8);
+    }
+
+    public static LauncherConfig LoadOrCreateDefault(string configPath)
+    {
+        LauncherConfig config;
+        if (!File.Exists(configPath))
+        {
+            config = GetDefaultConfig();
+        }
+        else
+        {
+            var configTextContent = File.ReadAllText(configPath, Encoding.UTF8);
+            var loadedJson = JsonSerializer.Deserialize<LauncherConfig>(configTextContent);
+            if (loadedJson != null)
+            {
+                config = loadedJson;
+            }
+            else
+            {
+                Console.WriteLine("Config is null after loading? Replacing it with default one");
+                config = GetDefaultConfig();
+            }
+        }
+
+        PatchConfigIfNeeded(config);
+        config.SaveConfig(configPath);
+
+        return config;
+    }
+
+    private static void PatchConfigIfNeeded(LauncherConfig config)
+    {
+        if (config.ConfigVersion < 2)
+        {
+            // For future use
+        }
+    }
+}
---
+++ WinterspringLauncher/Utils/UtilHelper.cs
@@ -0,0 +1,47 @@
+﻿namespace WinterspringLauncher.Utils;
+
+public static class UtilHelper
+{
+    // Converts some arbitrary byte number to binary human unit (1024 -> "1.00 KiB")
+    public static string ToHumanFileSize(long sizeInByte)
+    {
+        string[] units = { "Byte", "KiB", "MiB", "GiB", "TiB" };
+        int unitIdx = 0;
+
+        double size = sizeInByte;
+        while (size >= 1024 && unitIdx < units.Length - 1) {
+            unitIdx++;
+            size /= 1024;
+        }
+
+        string unit = units[unitIdx];
+        return $"{size:0.00} {unit}";
+    }
+
+    public static string ReplaceFirstOccurrence(this string source, string needle, string replacement, StringComparison comparison = StringComparison.InvariantCulture)
+    {
+        int pos = source.IndexOf(needle, comparison);
+        if (pos == -1)
+            return source;
+
+        string result = source.Remove(pos, needle.Length).Insert(pos, replacement);
+        return result;
+    }
+
+
+    public delegate bool CopyFolderFilterDelegate(string srcFileName);
+    
+    public static void CopyFolderRecursively(string srcFolder, string dstFolder, CopyFolderFilterDelegate? filter = null)
+    {
+        foreach (var srcFilePath in Directory.GetFiles(srcFolder, "*", SearchOption.AllDirectories))
+        {
+            var relPath = Path.GetRelativePath(srcFolder, srcFilePath);
+            var dstFilePath = Path.Combine(dstFolder, relPath);
+            if (filter?.Invoke(srcFilePath) ?? true)
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(dstFilePath)!);
+                File.Copy(srcFilePath, dstFilePath, overwrite: true);
+            }
+        }
+    }
+}
